40fb2427bf3c71ae4bfbd4f730693633edd6e40d
Michael <jmafoster1@gmail.com>
2021-09-24 08:07:59 +0100

    Added generic steps for simple IO DAG

--- causcumber/draw_dag_steps.py
+++ causcumber/draw_dag_steps.py
@@ -24,11 +24,24 @@ def step_impl(context):
         context.types[row['variable']] = locate(row['type'])
 
 
+@given(u'the following variables are recorded at the end of the simulation')
+def step_impl(context):
+    context.desired_outputs = [row['variable'] for row in context.table]
+    for row in context.table:
+        context.types[row['variable']] = locate(row['type'])
+
+
 @given(u'a connected repeating unit')
 def step_impl(context):
     inputs = list(context.params_dict.keys())
     context.repeating_unit = draw_connected_repeating_unit(inputs, context.desired_outputs)
     context.repeating_unit.write(f"dags/{context.feature_name}_repeating_unit.dot")
+
+
+@given(u'a connected DAG')
+def step_impl(context):
+    inputs = list(context.params_dict.keys())
+    context.repeating_unit = draw_connected_dag(inputs, context.desired_outputs)
 
 
 @when(u'we prune the following edges')
@@ -47,3 +60,8 @@ def step_impl(context):
 def step_impl(context, n, time_steps):
     dag = iterate_repeating_unit(context.repeating_unit, int(n), start=1)
     dag.write(context.dag_path)
+
+
+@then(u'we obtain the causal DAG')
+def step_impl(context):
+    context.repeating_unit.write(context.dag_path)
