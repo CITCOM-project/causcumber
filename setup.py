a2148688106cce769d3e6359cf644def5cbc7a58
Michael <jmafoster1@gmail.com>
2021-09-24 07:54:50 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='causcumber',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/CITCOM-project/causcumber',
     author='causcumber Team',
     packages=find_packages(),
