a51f23491c96b5831a2aeed1f5d9f5b3ac6f8eba
Stanley-deng <74106358+Stanley-deng@users.noreply.github.com>
2022-03-11 21:42:11 +0000

    Update help message for files in steps

--- gui/helpWindow.py
+++ gui/helpWindow.py
@@ -6,7 +6,16 @@ Step two:
 Edit the run_[model name] function in feature/environment.py in order to exectue the tested model
 
 Step three:
-Edit the file feature/steps to receive the parameters from the tested model
+Edit the files feature/steps to process the parameters from the tested model
+
+Custom Distributions                                          |
+User needs to define custom distrubutions for parameters. This is done in `dag_steps.py`.
+
+Metavariables
+User can also define metavariables in the Background. A metavariable called “m” needs a function “populate_m” in `dag_steps.py`.
+
+Custom Constraints
+Custom constraint need hook defining for them in abstract.py.
 
 Step four:
 Use the edit dot files function in the main screen to create a dot file that specify the relation between the varibles
